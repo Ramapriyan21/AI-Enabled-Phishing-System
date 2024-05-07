d7e02fb39c303861da961422b16d310692438b9e
Himasahana <60807277+hs1802@users.noreply.github.com>
2024-05-07 16:06:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('sms-email-spam-classifier-main/vectorizer.pkl','rb'))
-model = pickle.load(open('sms-email-spam-classifier-main/model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email/SMS Spam Classifier")
 
